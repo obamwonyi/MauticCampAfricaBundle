c51cffa99099743b779758af100f5045d2b5ecd6
obamwonyi <developer.oduwa@gmail.com>
2024-07-18 14:56:50 +0100

    Initialize the project

---
+++ Config/config.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+	'name'        => 'MauticCampAfrica',
+	'description' => 'Manage Mautic Camp events registrations in Africa/Lagos',
+	'author'      => [
+		[
+			'name' => 'Destiny Obamwonyi',
+			'email' => 'developer.oduwa@gmail.com'
+		],
+		[
+			'name' => '',
+			'email' => ''
+		]
+	],
+	'version'    => '1.0.0',
+	'services'   => [
+	],
+	'routes'	=> [
+
+	]
+];
---
+++ MauticCampAfricaBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MauticPlugin\MauticCampAfricaBundle;
+
+use Mautic\PluginBundle\Bundle\PluginBundleBase;
+
+class MauticCampAfricaBundle extends PluginBundleBase
+{
+}
